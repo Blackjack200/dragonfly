0ee11258368a64915f3a7ce2ada8766eae09f6c9
Blackjack200 <lx826444126@gmail.com>
2025-04-18 20:11:45 +0800

    Revert "add prefix for HandleChat"
    This reverts commit 518362aad2fd8d403a6de4a6394d274c4de7b768.

--- server/player/player.go
+++ server/player/player.go
@@ -324,11 +324,10 @@ func (p *Player) RemoveBossBar() {
 func (p *Player) Chat(msg ...any) {
 	message := format(msg)
 	ctx := event.C(p)
-	prefix := ""
-	if p.Handler().HandleChat(ctx, &prefix, &message); ctx.Cancelled() {
+	if p.Handler().HandleChat(ctx, &message); ctx.Cancelled() {
 		return
 	}
-	_, _ = fmt.Fprintf(chat.Global, "§7%v§r§7%v: %v\n", prefix, p.Name(), message)
+	_, _ = fmt.Fprintf(chat.Global, "§7%v: %v\n", p.Name(), message)
 }
 
 // ExecuteCommand executes a command passed as the player. If the command could not be found, or if the usage
