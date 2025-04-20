53c95c53df14a6e0b5e057bb885c29ffcae1ffd1
Blackjack200 <lx826444126@gmail.com>
2025-04-20 18:03:35 +0800

    Implement temporary panic recovery in handlePacket.

--- server/session/session.go
+++ server/session/session.go
@@ -22,6 +22,8 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"os"
+	"runtime"
 	"slices"
 	"sync"
 	"sync/atomic"
@@ -356,6 +358,22 @@ func (s *Session) handlePackets(tx *world.Tx, e Handlable) {
 	buf := slices.Clone(s.pkBuf)
 	s.pkBuf = nil
 	s.pkBufMu.Unlock()
+	defer func() {
+		if r := recover(); r != nil {
+			buf := make([]uintptr, 64)
+			n := runtime.Callers(1, buf)
+			frames := runtime.CallersFrames(buf[:n])
+
+			s.conf.Log.Error("panic: process packet: ", "err", r)
+			for {
+				frame, more := frames.Next()
+				_, _ = fmt.Fprintf(os.Stderr, "%s:%d %s\n", frame.File, frame.Line, frame.Function)
+				if !more {
+					break
+				}
+			}
+		}
+	}()
 	for _, pk := range buf {
 		err := s.handlePacket(pk, tx, e)
 		if err != nil {
